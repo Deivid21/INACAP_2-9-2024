df324eb2cf9e67d9a1184d46bc0d56559f0873c2
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-06 19:21:08 -0400

    Lab1: Improve and Bug Fixes [2/3]
    Change-Id: Ic4eb70821c04059ffbd5143f493695b3dfde314f

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,34 +1,48 @@
-void setup() {
-  // Pines
-  pinMode(2, INPUT); // LDR1
-  pinMode(3, INPUT); // LDR2
-  pinMode(4, INPUT); // Pulsador 1
-  pinMode(5, INPUT); // Pulsador 2
-  pinMode(13, OUTPUT); // LED 1
-  pinMode(12, OUTPUT); // LED 2
-  pinMode(11, OUTPUT); // LED 3
-  pinMode(10, OUTPUT); // LED 4
 
+void setup()
+{
+  // Pines
+  pinMode(13, OUTPUT); // LED 1 - Rojo
+  pinMode(12, OUTPUT); // LED 2 - Verde
+  pinMode(11, OUTPUT); // LED 3 - V/V 1
+  pinMode(10, OUTPUT); // LED 4 - V/V 2
+  pinMode(5, INPUT); // LDR2 - Maximo
+  pinMode(4, INPUT); // LDR1 - Minimo
+  pinMode(3, INPUT); // Pulsador 2 - Partida
+  pinMode(2, INPUT); // Pulsador 1 - Parada
+  digitalWrite(13, HIGH); // LED 1 - Rojo Encendido Inicio
 }
 
-void loop() {
-
-  // Control LED inicio y final
+void loop()
+{
+// Pulsador 1 - Parada
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+  }
+// Pulsador 2 - Partida
+  if (digitalRead(3) == HIGH) {
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+  }
+// LDR1 - Minimo
   if (digitalRead(4) == LOW) {
     digitalWrite(11, HIGH);
     digitalWrite(10, LOW);
-  }
-  if (digitalRead(5) == LOW) {
+  } else if (digitalRead(4) == HIGH) {
     digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+  }
+// LDR2 - Maximo
+  if (digitalRead(5) == LOW) {
     digitalWrite(10, HIGH);
-  }
-
-  // LDR
-  if (digitalRead(2) == LOW) {
-    digitalWrite(13, HIGH);
-  }
-
-  if (digitalRead(3) == LOW) {
-    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+  } else if (digitalRead(5) == HIGH) {
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
   }
 }
